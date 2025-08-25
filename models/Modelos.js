786a152eab0f09b60383aa34acb0f844325b2350
Gabriel-Comparini <gc77777777777777@gmail.com>
2025-08-25 17:34:22 -0300

    DbWithAlotOfBugs

--- models/Modelos.js
+++ models/Modelos.js
@@ -1,17 +1,21 @@
 import { Sequelize } from 'sequelize';
+import sqlite from 'sqlite3'
 import AnimalModel from './Animal.js';
-import TutorModel from './Tutor.js';
+//import TutorModel from './Tutor.js';
 import QuestionarioModel from './Questionario.js';
 import PedidoAdocaoModel from './PedidoAdocao.js';
 import DoacaoModel from './Doacao.js';
 
+const sqlite3 = sqlite.verbose();
+const db = new sqlite3.Database('database.db');
+
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './database.sqlite',
+    storage: './database.db',
 });
 
 export const Animal = AnimalModel(sequelize);
-export const Tutor = TutorModel(sequelize);
+//export const Tutor = TutorModel(sequelize);
 export const Questionario = QuestionarioModel(sequelize);
 export const PedidoAdocao = PedidoAdocaoModel(sequelize);
 export const Doacao = DoacaoModel(sequelize);
@@ -25,4 +29,5 @@ export const Doacao = DoacaoModel(sequelize);
 
 await sequelize.sync();
 
-export default { sequelize, Animal, Tutor, Questionario, PedidoAdocao, Doacao };
+//export default { sequelize, Animal, Tutor, Questionario, PedidoAdocao, Doacao };
+export default { sequelize, Animal, Questionario, PedidoAdocao, Doacao };
