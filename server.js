887299a93e0b7188aa99d18d135b16f256f38041
Gabriel-Comparini <gc77777777777777@gmail.com>
2025-09-01 16:48:43 -0300

    ServerCrud

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import sequelize from './models/Modelos.js';
+import { deleteAdmAnimais, getAdmAnimais, getAnimaisById, getAnimal, patchAdmAnimais, postAdocoes, postAnimal, postDoacoes, postLogin, postQuestionario, postTutores } from './routes.js';
 
 export const app = express();
 const port = 3000;
@@ -20,11 +21,51 @@ app.get('/', (req, res) => {
 });
 
 app.get("/animais", (req, res) => {
-    
+    getAnimal(req, res);
 });
 
 app.post('/animais', (req, res) => {
+    postAnimal(req, res);
+});
 
+app.post('/tutores/:id', (req, res) => {
+    postTutores(req, res);
+});
+
+app.get('/tutores/:id', (req, res) => {
+    postTutores(req, res);
+});
+
+app.post('/questionario', (req, res) => {
+    postQuestionario(req, res);
+});
+
+app.post('/adocoes', (req, res) => {
+    postAdocoes(req, res);
+});
+
+app.get('/admin/animais', (req, res) => {
+    getAdmAnimais(req, res);
+});
+
+app.patch('/admin/animais/:id', (req, res) => {
+    patchAdmAnimais(req, res);
+});
+
+app.delete('/admin/animais/:id', (req, res) => {
+    deleteAdmAnimais(req, res);
+});
+
+app.get('/animais/:id', (req, res) => {
+    getAnimaisById(req, res);
+});
+
+app.post('/login', (req, res) => {
+    postLogin(req, res);
+});
+
+app.post('/doacoes', (req, res) => {
+    postDoacoes(req, res);
 });
 
 app.listen(port, (err) => {
