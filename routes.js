222c8cc44f5603b0183528726be6fd0584717e8c
Gabriel-Comparini <gc77777777777777@gmail.com>
2025-09-01 16:29:47 -0300

    AddingRoutesFunctions

--- routes.js
+++ routes.js
@@ -1,12 +1,51 @@
-import {sequelize} from sequelize;
-import {express} from express;
+export async function getAnimal(req, res) {
 
-const app = express();
-const PORT = 3000;
+}
 
-app.use(express.json());
-
-app.get("/animais", (req, res) => {
+export async function postAnimal(req, res) {
     
-})
+}
 
+export async function postTutores(req, res) {
+    
+}
+
+export async function patchTutores(req, res) {
+    
+}
+
+export async function getTutores(req, res) {
+    
+}
+
+export async function postQuestionario(req, res) {
+    
+}
+
+export async function postAdocoes(req, res) {
+    
+}
+
+export async function getAdmAnimais(req, res) {
+    
+}
+
+export async function patchAdmAnimais(req, res) {
+    
+}
+
+export async function deleteAdmAnimais(req, res) {
+    
+}
+
+export async function getAnimaisById(req, res) {
+    
+}
+
+export async function postLogin(req, res) {
+    
+}
+
+export async function postDoacoes(req, res) {
+    
+}
