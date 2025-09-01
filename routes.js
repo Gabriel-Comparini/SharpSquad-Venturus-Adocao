d9d2b1a9297957ea81bd2df37592858e1898fdda
Fabricio Krull <fabricioakrull@gmail.com>
2025-09-01 17:08:37 -0300

    Testing the animalServices.js

--- routes.js
+++ routes.js
@@ -1,9 +1,20 @@
-export async function getAnimal(req, res) {
+import { createAnimal, findAllAnimals } from "./services/animalServices";
 
+export async function getAnimal(req, res) {
+    try {
+        return res.status(201).send(await findAllAnimals());
+    } catch (error) {
+        return res.status(500).send("Erro ao consultar animais");
+    }
 }
 
 export async function postAnimal(req, res) {
-    
+    try {
+        createAnimal(req.body);
+        return res.status(201).json(req.body);
+    } catch (error) {
+        return res.status(500).send("Erro ao criar animal");
+    }
 }
 
 export async function postTutores(req, res) {
